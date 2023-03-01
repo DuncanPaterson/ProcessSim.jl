0c9d7f0bc3a5e782c7ef015c5c044a02b1b6d8f8
Duncan Paterson <duncan_paterson@outlook.com>
2023-03-01 10:51:15 +0100

    adding used libs

--- src/ProcessSim.jl
+++ src/ProcessSim.jl
@@ -1,7 +1,10 @@
 
-using StaticArrays
-
 module ProcessSim
+
+using NLsolve
+using ForwardDiff
+using DiffResults
+
 
 include("PhysicalProperties/PhysicalProperty.jl")
 # Write your package code here.
