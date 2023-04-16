65bfadd1a62507219fad6122fa769970936d8977
Duncan Paterson <duncan_paterson@outlook.com>
2023-04-16 13:19:44 +0200

    Adding maths

--- src/ProcessSim.jl
+++ src/ProcessSim.jl
@@ -4,8 +4,10 @@ module ProcessSim
 using NLsolve
 using ForwardDiff
 using DiffResults
+using StaticArrays
 
 
+include("Maths/Maths.jl")
 include("PhysicalProperties/PhysicalProperty.jl")
 # Write your package code here.
 
